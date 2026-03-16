d58e0919d8e5495ebe41bb2291ba23f596f1694b
FjordPolaris <yorushika.lyrah@gmail.com>
2026-03-16 07:20:35 +0700

    Sync

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,94 @@
-#include <iostream>
+#define UNUSED __attribute__((unused))
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 const char WINDOW_TITLE[] = "Renderer OpenGL 4.1";
 const int WINDOW_SIZE[2] = { 1280, 720 };
 
-void create_window(void)
+// Window resizing
+void framebuffer_size_callback( UNUSED GLFWwindow *window, int width, int height )
+{
+	glViewport( 0, 0, width, height );
+}
+
+// Create window
+GLFWwindow* create_window( void )
 {
 	if ( !glfwInit() ) {
-		std::cout << "GLFW inita failed\n";
+		std::cout << "GLFW init failed\n";
+		return NULL;
 	}
 
 	// OpenGL Version Hints
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS Required
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE ); // macos
 
-	GLFWwindow *window = glfwCreateWindow(WINDOW_SIZE[0], WINDOW_SIZE[1],
-					      WINDOW_TITLE, NULL, NULL);
-	if (window == NULL) {
+	GLFWwindow *window = glfwCreateWindow( WINDOW_SIZE[0], WINDOW_SIZE[1],
+					      WINDOW_TITLE, NULL, NULL );
+	if ( !window ) {
 		std::cout << "Failed to create GLFW window\n";
-
-		return;
+		glfwTerminate();
+		return NULL;
 	}
-	glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent( window );
+	glfwSwapInterval(1);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	if ( !gladLoadGLLoader(( GLADloadproc ) glfwGetProcAddress )) {
 		std::cout << "Failed to initialise GLAD\n";
-
-		return;
+		return NULL;
 	}
 
-	glViewport(0, 0, 1280, 720);
-
-	// Render Loop
-	while(!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+	return window;
 }
 
+void process_input( GLFWwindow *window )
+{
+	if ( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS )
+		glfwSetWindowShouldClose( window, true );
+}
+
+// Triangle
+
+void draw_triangle( void )
+{
+	// x, y ,z
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f,  -0.5f, 0.0f,
+		0.0f,   0.5f, 0.0f
+	};
+
+	
+}
+
+// MAIN
 int main (void)
 {
-	create_window();
+	GLFWwindow *window = create_window();
+	if ( !window ) {
+		return EXIT_FAILURE;
+	}
+
+	glViewport( 0, 0, WINDOW_SIZE[0], WINDOW_SIZE[1] );
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render Loop
+	while( !glfwWindowShouldClose( window ) ) {
+		// input
+		process_input( window );
+
+		// commands
+
+		// clean
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers( window );
+		glfwPollEvents();
+	}
 
 	glfwTerminate();
-	return 0;
+	return EXIT_SUCCESS;
 }
