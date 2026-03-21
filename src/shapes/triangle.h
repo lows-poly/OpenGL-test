3d306bac6c12dcd8baf3fa6ff36ae9325bbe9711
FjordPolaris <yorushika.lyrah@gmail.com>
2026-03-22 05:42:45 +0700

    Change vertices order

--- src/shapes/triangle.h
+++ src/shapes/triangle.h
@@ -5,12 +5,12 @@
 namespace shape {
 	constexpr GLfloat tri_vert[] = {
 		// x    y     z        r     g     b
-		0.5f,  -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,
 		0.0f,   0.5f, 0.0f,    1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f
+		0.5f,  -0.5f, 0.0f,    0.0f, 1.0f, 0.0f
 	};
 
 	constexpr GLuint tri_ind[] = {
-		0, 1, 2
+		0, 2, 1
 	};
 }
